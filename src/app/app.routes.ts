ccfa14ef26859aa6d5cf3271ff3ea2a590b019a0
NguyenXuanTien <xuantien24689@gmail.com>
2026-04-03 20:21:37 +0700

    create source tree and home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent
+    }
+];
---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from "../../pages/home/home.component";
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [HomeComponent],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.css'
+})
+export class MainLayoutComponent {
+
+}
