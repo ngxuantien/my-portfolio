4664a588d644c16b57dab8016320993d1e56f0a5
DESKTOP-GURND3D\TIEN <xuantien24689@gmail.com>
2026-04-27 16:48:56 +0700

    update front root

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SectionService } from '../../core/services/section.service';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,25 @@ import { SectionService } from '../../core/services/section.service';
 })
 export class HomeComponent {
   constructor(private sectionService: SectionService) {}
+
+  ngAfterViewInit() {
+    new Typed('#typing', {
+      strings: [
+        'Full-Stack Software Engineer',
+        '.Net · Angular · Supabase',
+        'SaaS · Real-time Systems',
+      ],
+      typeSpeed: 50,
+      backSpeed: 0,
+      backDelay: 1500,
+      loop: true,
+      smartBackspace: true,
+      fadeOut: true,
+      fadeOutClass: 'typed-fade-out',
+      fadeOutDelay: 300,
+      showCursor: false
+    });
+  }
 
   scrollTo(id: string): void {
     this.sectionService.setActiveSection(id);
